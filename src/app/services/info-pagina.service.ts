b2720ff08175fee1da978d3419418fe7c0b331b4
pulgar.henry.HP@gmail.com <pulgar.henry.HP@gmail.com>
2022-12-06 11:28:01 -0400

    Comitiar al 06-12-2022

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,13 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class InfoPaginaService {
 
-  constructor() { 
- 
-    console.log("SERVICIO DE infoPagina"); 
+    constructor( private http: HttpClient ) { 
 
-  }
-}
+      console.log("SERVICIO DE infoPagina");  
+      // Leer archivo .json
+      this.http.get('assets/data/data-pagina.json')
+        .subscribe( resp => { console.log( resp ); }  )
+
+} }
