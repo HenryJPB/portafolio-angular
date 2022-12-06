1919ceb293aa6b27c1dc69185ed5c0169ffa16d9
pulgar.henry.HP@gmail.com <pulgar.henry.HP@gmail.com>
2022-12-06 10:09:45 -0400

    Comitiar al 06-12-2022

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor() { 
+ 
+    console.log("SERVICIO DE infoPagina"); 
+
+  }
+}
