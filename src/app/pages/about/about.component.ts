8d3b197aaf4615383d8dae3bc6ddaf22718ebc33
pulgar.henry.HP@gmail.com <pulgar.henry.HP@gmail.com>
2022-12-12 16:09:36 -0400

    commit al 12-12-22 16:07

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _infoService: InfoPaginaService,
+               public _productosService: ProductosService ) { }
 
   ngOnInit(): void {
   }
