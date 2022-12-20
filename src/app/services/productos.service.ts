898952f5e1e113130ba86c6d71cab639e7ee2fd1
pulgar.henry.HP@gmail.com <pulgar.henry.HP@gmail.com>
2022-12-20 09:26:44 -0400

    Backup al 20-12-2022 09:25

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -29,4 +29,9 @@ export class ProductosService {
              */ 
       }  );  
   }
+
+  getProducto( id: string ) {
+      return this.http.get(`https://angular-html-4a1ec-default-rtdb.firebaseio.com/productos/${ id }.json`);  
+  } // getProducto.
+
 }
